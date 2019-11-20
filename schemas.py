529d6b42c9be39993b81d89b145c2db7c1b77202
y-839829144 <57943829+y-839829144@users.noreply.github.com>
2019-11-20 09:36:50 +0800

    alarm
    告警管理schema

--- schemas.py
+++ schemas.py
@@ -14,40 +14,133 @@ TokenSchema = AutoSchema([
         ]
 )
 
-
-AccessUpdateSchema = AutoSchema([
+AlarmManySchema = AutoSchema([
     # token_field,
     coreapi.Field(
-                "Door_approval",
+                "Type_id_id",
                 required=True,
-                location="form",# form
-                schema=coreschema.Object(),
+                location="query",# form
+                schema=coreschema.Integer(),
                 # schema=coreschema.String(),
-                description="申请审核",
+                description="类型id",
             ),
+
+
 
 ])
-CountSchema = AutoSchema([
+AlarmManySchema1 = AutoSchema([
     # token_field,
-    coreapi.Field(
-                "Count",
+coreapi.Field(
+                "am_id",
                 required=True,
-                location="form",# form
-                schema=coreschema.Object(),
+                location="query",# form
+                schema=coreschema.Anything,
                 # schema=coreschema.String(),
-                description="各用户申请开门次数占比",
+                description="处理id",
             ),
+coreapi.Field(
+                "am_deal_detail",
+                required=True,
+                location="query",# form
+                schema=coreschema.String(),
+                # schema=coreschema.String(),
+                description="处理内容",
+            ),
+
 
 ])
-DoorTimeSchema = AutoSchema([
+
+AlarmApplicationSchema = AutoSchema([
     # token_field,
     coreapi.Field(
-                "Time",
+                "alarm_id",
+                required=True,
+                location="query",# form
+                schema=coreschema.Integer(),
+                # schema=coreschema.String(),
+                description="告警id",
+            ),
+coreapi.Field(
+                "alarm_dealtail",
+                required=True,
+                location="query",# form
+                schema=coreschema.String(),
+                # schema=coreschema.String(),
+                description="告警处理说明",
+            ),
+
+
+
+])
+
+AlarmAuditSchema = AutoSchema([
+    # token_field,
+    coreapi.Field(
+                "alarm_id",
+                required=True,
+                location="query",# form
+                schema=coreschema.Integer(),
+                # schema=coreschema.String(),
+                description="告警ID",
+            ),
+coreapi.Field(
+                "am_audit_detail",
+                required=True,
+                location="query",# form
+                schema=coreschema.String(),
+                # schema=coreschema.String(),
+                description="审批说明",
+            ),
+
+
+
+])
+
+AlarmTypeCountSchema = AutoSchema([
+    # token_field,
+    coreapi.Field(
+                "input_time",
                 required=True,
                 location="form",# form
                 schema=coreschema.Object(),
                 # schema=coreschema.String(),
-                description="各用户开门总时间占比",
+                description="类型统计占比",
             ),
 
+
+
+])
+
+SafeCountSchema = AutoSchema([
+    # token_field,
+coreapi.Field(
+                "type_id",
+                required=True,
+                location="query",# form
+                schema=coreschema.Integer(),
+                # schema=coreschema.String(),
+                description="安防类型ID",
+            ),
+    coreapi.Field(
+                "input_time",
+                required=True,
+                location="form",# form
+                schema=coreschema.Object(),
+                # schema=coreschema.String(),
+                description="安防监测告警各状态数量占比",
+            ),
+
+
+
+])
+AlarmTypePrecentSchema = AutoSchema([
+    # token_field,
+    coreapi.Field(
+                "alarm_time",
+                required=True,
+                location="form",  # from,query,url
+                schema=coreschema.Object(),
+                # schema=coreschema.String(),
+                description="告警时间段",
+            ),
 ])
